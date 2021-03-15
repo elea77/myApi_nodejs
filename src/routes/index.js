5fb787e5abee1072fa47e6bd3d4859d841d96036
eleacarton <eleacarton@me.com>
2021-03-15 12:11:32 +0100

    add router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const userRouter = require('./user.route');
 const productRouter = require('./product.route');
 const orderRouter = require('./order.route');
+const categoryRouter = require('./category.route');
 
 router.use(userRouter);
 router.use(productRouter);
 router.use(orderRouter);
+router.use(categoryRouter);
 
 module.exports = router;
