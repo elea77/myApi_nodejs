5c0fb51e675b5ece852391bddd81d9d86d0e6c47
eleacarton <eleacarton@me.com>
2021-03-15 12:17:37 +0100

    getAll categories

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -24,3 +24,17 @@ exports.create = (req, res) => {
     })
 
 }
+
+exports.getAll = (req, res) => {
+    Category.find().then(
+        (data) => {
+          res.status(200).json(data);
+        }
+    ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+    );
+}
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const category = require('../controllers/categories.controller');
 
 router.post('/categories', category.create);
+router.get('/categories', category.getAll);
 
 module.exports = router;
