231259a69536f81d73f127cb7ea9e118b2d2b9ab
eleacarton <eleacarton@me.com>
2021-03-23 16:34:57 +0100

    stripe

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const userRouter = require('./user.route');
 const productRouter = require('./product.route');
 const orderRouter = require('./order.route');
 const categoryRouter = require('./category.route');
+const checkoutRouter = require('./checkout.route');
 
 router.use(userRouter);
 router.use(productRouter);
 router.use(orderRouter);
 router.use(categoryRouter);
+router.use(checkoutRouter);
 
 module.exports = router;
